25fbf9ce7f4b9abe3f04f4cf59bc59854cd1e8e9
leangaurav <leanguarav.me@gmail.com>
2022-12-26 23:06:35 +0530

    Reduce once interface for V2

--- once_test.go
+++ once_test.go
@@ -8,521 +8,154 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func returnTrue() bool  { return true }
-func returnFalse() bool { return false }
-func doPanic() bool     { panic(1); return false }
-func returnTrueWithDelay(t time.Duration) func() bool {
-	return func() bool { time.Sleep(t); return true }
+type one int
+
+func (o *one) Increment() {
+	*o++
 }
-func returnFalseWithDelay(t time.Duration) func() bool {
-	return func() bool { time.Sleep(t); return false }
+
+func (o *one) Compare(value int) bool {
+	return int(*o) == value
 }
+
+func sleeper(d time.Duration, o *one) func() {
+	return func() { time.Sleep(d); o.Increment() }
+}
+
+func doPanic() { panic(1) }
 
 func TestDefaults(t *testing.T) {
-	o, err := NewDefaultOnce(returnTrue)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, o.Done(false), false)
-	assert.Equal(t, o.Do(), true)
-	assert.Equal(t, o.Done(false), true)
-	assert.Equal(t, o.Do(), false)
-}
 
-func TestOnceValidArgumentCombinations(t *testing.T) {
-	var err error
-	_, err = NewOnce(false, false, VerifyNone, returnTrue)
-	assert.Equal(t, err, nil)
+	o := new(one)
+	once := NewOnce(false, sleeper(time.Millisecond*5, o))
 
-	_, err = NewOnce(false, false, VerifyAll, returnTrue)
-	assert.NotEqual(t, err, nil)
+	assert.Equal(t, once.Done(false), false)
+	assert.True(t, once.Do())
+	assert.False(t, o.Compare(0))
 
-	_, err = NewOnce(false, false, VerifyFirstExit, returnTrue)
-	assert.NotEqual(t, err, nil)
-
-	_, err = NewOnce(false, false, VerifyFirstRunAll, returnTrue)
-	assert.NotEqual(t, err, nil)
-
-	_, err = NewOnce(true, false, VerifyNone, returnTrue)
-	assert.Equal(t, err, nil)
-
-	_, err = NewOnce(true, false, VerifyAll, returnTrue)
-	assert.Equal(t, err, nil)
-
-	_, err = NewOnce(true, false, VerifyFirstExit, returnTrue)
-	assert.Equal(t, err, nil)
-
-	_, err = NewOnce(true, false, VerifyFirstRunAll, returnTrue)
-	assert.Equal(t, err, nil)
-}
-
-func TestLazyDone(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	o, err = NewOnce(false, false, VerifyNone, returnTrueWithDelay(time.Millisecond*4))
-	assert.Equal(t, err, nil)
-	go func() { assert.Equal(t, true, o.Do()) }()
-	time.Sleep(time.Millisecond)
-	assert.Equal(t, o.Done(false), true)
-
-	o, err = NewOnce(true, false, VerifyNone, returnTrueWithDelay(time.Millisecond*1))
-	assert.Equal(t, err, nil)
-	go func() { assert.Equal(t, true, o.Do()) }()
-	assert.Equal(t, false, o.Done(false))
-	time.Sleep(time.Millisecond * 2)
-	assert.Equal(t, o.Done(false), true)
+	assert.Equal(t, once.Done(false), true)
+	assert.False(t, once.Do())
+	assert.True(t, o.Compare(1))
 }
 
 func TestPanicOption(t *testing.T) {
 	var (
-		err error
-		o   *Once
+		once Once
 	)
 
-	o, err = NewOnce(false, false, VerifyNone, doPanic)
-	assert.Equal(t, err, nil)
-	assert.Panics(t, func() { o.Do() })
+	once = NewOnce(false, doPanic)
+	assert.Panics(t, func() { assert.True(t, once.Do()) })
 
-	o, err = NewOnce(false, true, VerifyNone, doPanic)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, true, o.Do()) })
+	once = NewOnce(true, doPanic)
+	assert.NotPanics(t, func() { assert.True(t, once.Do()) })
 }
 
 func TestDoneAfterPanic(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
+	var o Once
 
-	// eager done with panic
-	o, err = NewOnce(false, false, VerifyNone, doPanic)
-	assert.Equal(t, err, nil)
+	// done with panic
+	o = NewOnce(false, doPanic)
 	assert.Panics(t, func() { o.Do() })
+	time.Sleep(time.Millisecond)
 	assert.Equal(t, o.Done(false), true)
 
-	// eager done with suppressed panic
-	o, err = NewOnce(false, true, VerifyNone, doPanic)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, true, o.Do()) })
+	// done with suppressed panic
+	o = NewOnce(true, doPanic)
+	assert.NotPanics(t, func() { o.Do() })
+	time.Sleep(time.Millisecond)
 	assert.Equal(t, o.Done(false), true)
-
-	// lazy done with panic
-	o, err = NewOnce(true, false, VerifyNone, doPanic)
-	assert.Equal(t, err, nil)
-	assert.Panics(t, func() { o.Do() })
-	assert.Equal(t, o.Done(false), false)
-
-	// lazy done with suppressed panic
-	o, err = NewOnce(true, true, VerifyNone, doPanic)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, true, o.Do()) })
-	assert.Equal(t, o.Done(false), false)
-}
-
-func TestVerifyNone(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	executed := false
-	f := func() bool { executed = true; return false }
-	o, err = NewOnce(true, false, VerifyNone, returnFalse, f)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-	assert.Equal(t, true, executed)
-}
-
-func TestLazyPanicVerifyNone(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	executed := false
-	f := func() bool { executed = true; return false }
-	o, err = NewOnce(false, false, VerifyNone, returnFalse, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.Panics(t, func() { o.Do() })
-	assert.Equal(t, true, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, true, VerifyNone, doPanic)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, true, o.Do()) })
-	assert.Equal(t, o.Done(false), false)
-	assert.Equal(t, false, executed)
-}
-
-func TestVerifyAll(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	executed := false
-	f := func() bool { executed = true; return false }
-	o, err = NewOnce(true, false, VerifyAll, returnTrue, f)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Do())
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, true, executed)
-
-	o, err = NewOnce(true, false, VerifyAll, returnFalse, returnTrue)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Do())
-	assert.Equal(t, false, o.Done(false))
-
-	o, err = NewOnce(true, false, VerifyAll, returnTrue, returnFalse)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Do())
-	assert.Equal(t, false, o.Done(false))
-
-	o, err = NewOnce(true, false, VerifyAll, returnTrue, returnTrue)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-}
-
-func TestPanicVerifyAll(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	executed := false
-	f := func() bool { executed = true; return false }
-	o, err = NewOnce(true, false, VerifyAll, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.Panics(t, func() { o.Do() })
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, true, VerifyAll, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, false, o.Do()) })
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, executed)
-}
-
-func TestVerifyFirstRunAll(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	executed := false
-	f := func() bool { executed = true; return false }
-	o, err = NewOnce(true, false, VerifyFirstRunAll, returnFalse, f)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, o.Do())
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, true, executed)
-
-	o, err = NewOnce(true, false, VerifyFirstRunAll, returnFalse, returnFalse)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Do())
-	assert.Equal(t, false, o.Done(false))
-
-	o, err = NewOnce(true, false, VerifyFirstRunAll, returnTrue, returnFalse)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-
-	o, err = NewOnce(true, false, VerifyFirstRunAll, returnFalse, returnTrue)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-
-	o, err = NewOnce(true, false, VerifyFirstRunAll, returnTrue, returnTrue)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-}
-
-func TestPanicVerifyFirstRunAll(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	executed := false
-	f := func() bool { executed = true; return false }
-	o, err = NewOnce(true, false, VerifyFirstRunAll, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.Panics(t, func() { o.Do() })
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, true, VerifyFirstRunAll, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, false, o.Do()) })
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, false, VerifyFirstRunAll, returnTrue, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.Panics(t, func() { o.Do() })
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, true, VerifyFirstRunAll, returnTrue, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, true, o.Do()) })
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, executed)
-}
-
-func TestVerifyFirstExit(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	executed := false
-	f := func() bool { executed = true; return false }
-
-	o, err = NewOnce(true, false, VerifyFirstExit, returnTrue, f)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, false, VerifyFirstExit, returnFalse, f, returnTrue)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-	assert.Equal(t, true, executed)
-
-	executed = false
-	o, err = NewOnce(true, false, VerifyFirstExit, returnFalse, f)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Do())
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, true, executed)
-}
-
-func TestPanicVerifyFirstExit(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	executed := false
-	f := func() bool { executed = true; return false }
-	o, err = NewOnce(true, false, VerifyFirstExit, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.Panics(t, func() { o.Do() })
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, true, VerifyFirstExit, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, false, o.Do()) })
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, false, VerifyFirstExit, returnTrue, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, true, o.Do()) })
-	assert.Equal(t, true, o.Done(false))
-	assert.Equal(t, false, executed)
-
-	executed = false
-	o, err = NewOnce(true, true, VerifyFirstExit, returnTrue, doPanic, f)
-	assert.Equal(t, err, nil)
-	assert.NotPanics(t, func() { assert.Equal(t, true, o.Do()) })
-	assert.Equal(t, true, o.Done(false))
-	assert.Equal(t, false, executed)
 }
 
 func TestBlockingGoroutines(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
+	var once Once
+	o := new(one)
 
 	ts := time.Now()
-	o, err = NewOnce(true, false, VerifyNone, returnTrueWithDelay(time.Millisecond*10))
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Done(false))
-	go func() { assert.Equal(t, true, o.Do()) }()
+	once = NewOnce(false, sleeper(time.Millisecond*20, o))
+
+	assert.Equal(t, false, once.Done(false))
+	go func() { assert.Equal(t, true, once.Do()) }()
+
 	time.Sleep(time.Millisecond)
-	assert.Equal(t, false, o.Done(false))
-	go func() { assert.Equal(t, false, o.Do()) }()
-	assert.True(t, time.Now().Sub(ts) < time.Millisecond*5)
-	assert.Equal(t, false, o.Do())
-	assert.True(t, time.Now().Sub(ts) > time.Millisecond*10)
-	assert.Equal(t, true, o.Done(false))
-}
 
-func TestReset(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
+	go func() { assert.Equal(t, false, once.Do()) }()
+	assert.Equal(t, false, once.Done(false))
+	assert.True(t, time.Since(ts) < time.Millisecond*20)
 
-	o, err = NewOnce(true, false, VerifyNone, returnTrue)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, false, o.Reset())
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-	assert.Equal(t, false, o.Do())
-
-	assert.Equal(t, true, o.Reset())
-	assert.Equal(t, false, o.Reset())
-	assert.Equal(t, false, o.Done(false))
-	assert.Equal(t, true, o.Do())
-	assert.Equal(t, true, o.Done(false))
-}
-
-func TestResetConcurrentBlocks(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	ts := time.Now()
-	o, err = NewOnce(true, false, VerifyNone, returnTrueWithDelay(time.Millisecond*4))
-	assert.Equal(t, err, nil)
-	go func() { assert.Equal(t, true, o.Do()) }()
-	assert.True(t, time.Now().Sub(ts) < time.Millisecond)
-	time.Sleep(time.Millisecond) // let Do() lock the mutex
-	go func() { assert.Equal(t, true, o.Reset()) }()
-	assert.True(t, time.Now().Sub(ts) < time.Millisecond*2)
-	time.Sleep(time.Millisecond * 4)
-	assert.Equal(t, false, o.Done(false))
+	// wait for Do to finish
+	assert.Equal(t, false, once.Do())
+	assert.True(t, time.Since(ts) > time.Millisecond*20)
+	assert.Equal(t, true, once.Done(false))
+	assert.True(t, o.Compare(1))
 }
 
 func TestBlockingDoneOneGoroutine(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
 
 	ts := time.Now()
-	o, err = NewOnce(true, false, VerifyAll, returnTrueWithDelay(time.Millisecond*4))
-	assert.Equal(t, err, nil)
-	go func() { assert.Equal(t, true, o.Do()) }()
+	o := new(one)
+	once := NewOnce(false, sleeper(time.Millisecond*10, o))
+	go func() { assert.Equal(t, true, once.Do()) }()
 
 	// call Done with block=true
-	assert.Equal(t, true, o.Done(true))
-	assert.True(t, time.Now().Sub(ts) > time.Millisecond*4)
+	assert.Equal(t, true, once.Done(true))
+	assert.True(t, time.Since(ts) > time.Millisecond*10)
+	assert.True(t, o.Compare(1))
 }
 
 func TestBlockingDoneMultipleGoroutine(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
 
+	o := new(one)
+	once := NewOnce(false, sleeper(time.Millisecond*4, o))
 	ts := time.Now()
-	o, err = NewOnce(true, false, VerifyAll, returnTrueWithDelay(time.Millisecond*4))
-	assert.Equal(t, err, nil)
-	go func() { assert.Equal(t, true, o.Do()) }()
+	go func() { assert.Equal(t, true, once.Do()) }()
 
 	// call Done with block=true
 	var t1, t2 time.Time
 	var wg sync.WaitGroup
-	go func() { wg.Add(1); assert.Equal(t, true, o.Done(true)); t1 = time.Now(); wg.Done() }()
-	go func() { wg.Add(1); assert.Equal(t, true, o.Done(true)); t2 = time.Now(); wg.Done() }()
+	wg.Add(1)
+	go func() { assert.Equal(t, true, once.Done(true)); t1 = time.Now(); wg.Done() }()
+	wg.Add(1)
+	go func() { assert.Equal(t, true, once.Done(true)); t2 = time.Now(); wg.Done() }()
 
 	// block for done to be set
-	o.Done(true)
+	once.Done(true)
 	wg.Wait()
 	assert.True(t, t2.Sub(ts) > time.Millisecond*4)
 	assert.True(t, t1.Sub(ts) > time.Millisecond*4)
 
-	assert.True(t, o.Done(false))
+	assert.True(t, once.Done(false))
 	// once state is DONE, Done(true) should return immediately
 	ts = time.Now()
-	assert.True(t, o.Done(true))
-	assert.True(t, time.Now().Sub(ts) < time.Microsecond*10)
+	assert.True(t, once.Done(true))
+	assert.True(t, time.Since(ts) < time.Millisecond*10)
+	assert.True(t, o.Compare(1))
 }
 
 func TestBlockingDoneMultipleGoroutineExplicitClose(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
+
+	var t1, t2 time.Time
+	var wg sync.WaitGroup
+
+	o := new(one)
+	once := NewOnce(false, sleeper(time.Millisecond*50, o))
 
 	ts := time.Now()
-	o, err = NewOnce(true, false, VerifyAll, returnFalse)
-	assert.Equal(t, err, nil)
-	go func() { assert.Equal(t, false, o.Do()) }()
+	go func() { assert.Equal(t, true, once.Do()) }()
 
 	// call Done with block=true
-	var t1, t2 time.Time
-
-	var wg sync.WaitGroup
-	wg.Add(1) // first goroutine calls wg.Done() after calling Add. this avoids this test case having a data race for the waitgroup
-	go func() { wg.Add(1); wg.Done(); assert.Equal(t, false, o.Done(true)); t1 = time.Now(); wg.Done() }()
-	go func() { wg.Add(1); assert.Equal(t, false, o.Done(true)); t2 = time.Now(); wg.Done() }()
+	wg.Add(1)
+	go func() { assert.Equal(t, false, once.Done(true)); t1 = time.Now(); wg.Done() }()
+	wg.Add(1)
+	go func() { assert.Equal(t, false, once.Done(true)); t2 = time.Now(); wg.Done() }()
 
 	// close after 5ms
-	go func() { wg.Add(1); time.Sleep(time.Millisecond * 5); o.Close(); wg.Done() }()
+	wg.Add(1)
+	go func() { time.Sleep(time.Millisecond * 5); once.Close(); wg.Done() }()
 
 	wg.Wait()
 
 	// both goroutines should get unblocked immediately after Close()
-	assert.True(t, time.Millisecond*5 < t2.Sub(ts) && t2.Sub(ts) < time.Millisecond*8)
-	assert.True(t, time.Millisecond*5 < t1.Sub(ts) && t1.Sub(ts) < time.Millisecond*8)
-}
-
-func TestBlockingDoneAfterReset(t *testing.T) {
-	var (
-		err error
-		o   *Once
-	)
-
-	ts := time.Now()
-	o, err = NewOnce(true, false, VerifyAll, returnTrueWithDelay(time.Millisecond*4))
-	assert.Equal(t, err, nil)
-	/*
-		Step-1
-	*/
-	go func() { assert.Equal(t, true, o.Do()) }()
-
-	// call Done with block=true
-	var t1, t2 time.Time
-	var wg sync.WaitGroup
-	go func() { wg.Add(1); assert.Equal(t, true, o.Done(true)); t1 = time.Now(); wg.Done() }()
-	go func() { wg.Add(1); assert.Equal(t, true, o.Done(true)); t2 = time.Now(); wg.Done() }()
-
-	// block for done to be set
-	o.Done(true)
-	wg.Wait()
-	time.Sleep(time.Millisecond) // wait for t1 and t2 to be set
-	assert.True(t, t2.Sub(ts) > time.Millisecond*4)
-	assert.True(t, t1.Sub(ts) > time.Millisecond*4)
-
-	/*
-		Step-2: re-test
-	*/
-	assert.True(t, o.Reset())
-	assert.False(t, o.Done(false))
-	go func() { assert.Equal(t, true, o.Do()) }()
-
-	// call Done with block=true
-	go func() { wg.Add(1); assert.Equal(t, true, o.Done(true)); t1 = time.Now(); wg.Done() }()
-	go func() { wg.Add(1); assert.Equal(t, true, o.Done(true)); t2 = time.Now(); wg.Done() }()
-
-	// block for done to be set
-	o.Done(true)
-	wg.Wait()
-	time.Sleep(time.Millisecond) // wait for t1 and t2 to be set
-	assert.True(t, t2.Sub(ts) > time.Millisecond*4)
-	assert.True(t, t1.Sub(ts) > time.Millisecond*4)
+	assert.True(t, time.Millisecond*5 < t2.Sub(ts) && t2.Sub(ts) < time.Millisecond*40)
+	assert.True(t, time.Millisecond*5 < t1.Sub(ts) && t1.Sub(ts) < time.Millisecond*40)
+	time.Sleep(time.Millisecond * 50)
 }
